c80933f225b904d6fcd8848230fdae9060fdd0ea
divyampandya63 <divyampandya63@gmail.com>
2019-01-23 20:37:15 +0530

    chatdialogue adapter added

---
+++ app/src/main/java/chat/divyam/com/chatdroid/Adapter/ChatDialogsAdapter.java
@@ -0,0 +1,84 @@
+package chat.divyam.com.chatdroid.Adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ChatDialogsAdapter {
+
+    private Context context;
+    private ArrayList<QBChatDialog> qbChatDialogs;
+    TextView txtTitle, txtMessage;
+    ImageView imageView;
+    LayoutInflater inflater;
+
+    public ChatDialogsAdapter(Context context, ArrayList<QBChatDialog> qbChatDialogs) {
+        this.context = context;
+        this.qbChatDialogs = qbChatDialogs;
+    }
+
+
+    @Override
+    public int getCount() {
+        return qbChatDialogs.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return qbChatDialogs.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null){
+            inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.list_chat_dialogs, null);
+
+            TextView txtTitle, txtMessage;
+            ImageView imageView, imageUnread;
+
+            txtMessage = (TextView)view.findViewById(R.id.list_chat_dialog_message);
+            txtTitle = (TextView)view.findViewById(R.id.list_chat_dialog_title);
+            imageView = (ImageView)view.findViewById(R.id.image_chatDialog);
+            imageUnread = (ImageView)view.findViewById(R.id.image_unRead);
+
+            txtMessage.setText(qbChatDialogs.get(position).getLastMessage());
+            txtTitle.setText(qbChatDialogs.get(position).getName());
+
+            ColorGenerator generator = ColorGenerator.MATERIAL;
+            int randomColor = generator.getRandomColor();
+
+            TextDrawable.IBuilder builder = TextDrawable.builder().beginConfig()
+                    .withBorder(4)
+                    .endConfig()
+                    .round();
+
+//            Lay chu cai dau tien trong Dialog chat de tao avatar chat
+            TextDrawable drawable = builder.build(txtTitle.getText().toString().substring(0, 1).toUpperCase(), randomColor);
+            imageView.setImageDrawable(drawable);
+
+            TextDrawable.IBuilder unreadBuilder = TextDrawable.builder().beginConfig()
+                    .withBorder(4)
+                    .endConfig()
+                    .round();
+            int unread_count = QBUnreadMessageHolder.getInstance().getBundle().getInt(qbChatDialogs.get(position).getDialogId());
+            if (unread_count > 0){
+                TextDrawable unread_drawable = builder.build(""+unread_count, Color.RED);
+                imageUnread.setImageDrawable(unread_drawable);
+            }
+        }
+        return view;
+    }
+}
