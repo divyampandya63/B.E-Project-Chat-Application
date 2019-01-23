0488318e38245505a7250044777586d316d4315e
divyampandya63 <divyampandya63@gmail.com>
2019-01-23 18:18:24 +0530

    Quickbox keys added

--- app/src/main/java/chat/divyam/com/chatdroid/MainActivity.java
+++ app/src/main/java/chat/divyam/com/chatdroid/MainActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String APP_ID = "75579";
+    static final String AUTH_KEY = "Gb9UkArNMAgujQN";
+    static final String AUTH_SECRET = "df3LNdrbqSaUUpy";
+    static final String ACCOUNT_KEY = "4dy7j9163RfnB7xb3ZYp";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
