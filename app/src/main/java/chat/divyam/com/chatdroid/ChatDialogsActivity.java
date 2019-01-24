c042053e1a365b08bced1b39cb6f8be2eee7be58
divyampandya63 <divyampandya63@gmail.com>
2019-01-24 17:16:04 +0530

    Chatdialog activity code added

--- app/src/main/java/chat/divyam/com/chatdroid/ChatDialogsActivity.java
+++ app/src/main/java/chat/divyam/com/chatdroid/ChatDialogsActivity.java
@@ -1,24 +1,41 @@
 package chat.divyam.com.chatdroid;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toolbar;
+import android.widget.ProgressBar;
 
+import com.androidprochatapplication.Adapter.ChatDialogsAdapter;
+import com.androidprochatapplication.Common.Common;
+import com.androidprochatapplication.Holder.QBChatDialogHolder;
+import com.androidprochatapplication.Holder.QBUnreadMessageHolder;
+import com.androidprochatapplication.Holder.QBUsersHolder;
 import com.quickblox.auth.QBAuth;
 import com.quickblox.auth.session.BaseService;
 import com.quickblox.auth.session.QBSession;
+import com.quickblox.auth.session.QBSettings;
 import com.quickblox.chat.QBChatService;
 import com.quickblox.chat.QBIncomingMessagesManager;
 import com.quickblox.chat.QBRestChatService;
 import com.quickblox.chat.QBSystemMessagesManager;
+import com.quickblox.chat.exception.QBChatException;
+import com.quickblox.chat.listeners.QBChatDialogMessageListener;
+import com.quickblox.chat.listeners.QBSystemMessageListener;
 import com.quickblox.chat.model.QBChatDialog;
+import com.quickblox.chat.model.QBChatMessage;
 import com.quickblox.core.QBEntityCallback;
 import com.quickblox.core.exception.BaseServiceException;
 import com.quickblox.core.exception.QBResponseException;
+import com.quickblox.core.request.QBRequestBuilder;
 import com.quickblox.core.request.QBRequestGetBuilder;
 import com.quickblox.users.QBUsers;
 import com.quickblox.users.model.QBUser;
@@ -27,29 +44,131 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-import chat.divyam.com.chatdroid.Adapter.ChatDialogsAdapter;
-
-public class ChatDialogsActivity extends AppCompatActivity {
-
+public class ChatDialogsActivity extends AppCompatActivity implements QBSystemMessageListener, QBChatDialogMessageListener {
     FloatingActionButton floatingActionButton;
     ListView firstChatDialogs;
     QBChatDialog qbChatDialog;
     Toolbar toolbar;
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        loadChatDialogs();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_dialogs);
 
-        createSessionForChat();
+        firstChatDialogs = (ListView)findViewById(R.id.firstChatDialogs);
+        firstChatDialogs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                qbChatDialog = (QBChatDialog)firstChatDialogs.getAdapter().getItem(position);
+                Intent intent = new Intent(ChatDialogsActivity.this, ChatMessageActivity.class);
+                intent.putExtra(Common.DIALOG_EXTRA, qbChatDialog);
+                startActivity(intent);
+            }
+        });
+
+
+
+        firstChatDialogs = (ListView)findViewById(R.id.firstChatDialogs);
+        firstChatDialogs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                qbChatDialog = (QBChatDialog)firstChatDialogs.getAdapter().getItem(position);
+                Intent intent = new Intent(ChatDialogsActivity.this, ChatMessageActivity.class);
+                intent.putExtra(Common.DIALOG_EXTRA, qbChatDialog);
+                startActivity(intent);
+            }
+        });
 
         loadChatDialogs();
+
+        createSessionForChat();
+
+        floatingActionButton = (FloatingActionButton)findViewById(R.id.chatDialogs_addUser);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChatDialogsActivity.this, ListUsersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        toolbar = (Toolbar)findViewById(R.id.chat_dialog_toolbar);
+        toolbar.setTitle("Android Pro Chat Application");
+        setSupportActionBar(toolbar);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.chat_dialog_menu, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.chat_dialog_menu_user:
+                showUserProfile();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    private void showUserProfile() {
+        Intent intent = new Intent(ChatDialogsActivity.this, UserProfile.class);
+        startActivity(intent);
+
+    }
+
+    private void loadChatDialogs() {
+        QBRequestGetBuilder requestBuilder = new QBRequestGetBuilder();
+        requestBuilder.setLimit(100);
+
+        QBRestChatService.getChatDialogs(null, requestBuilder).performAsync(new QBEntityCallback<ArrayList<QBChatDialog>>() {
+            @Override
+            public void onSuccess(ArrayList<QBChatDialog> qbChatDialogs, Bundle bundle) {
+                QBChatDialogHolder.getInstance().putDialogs(qbChatDialogs);
+
+                Set<String> setIds = new HashSet<String>();
+                for (QBChatDialog chatDialog:qbChatDialogs)
+                    setIds.add(chatDialog.getDialogId());
+
+                QBRestChatService.getTotalUnreadMessagesCount(setIds, QBUnreadMessageHolder.getInstance().getBundle())
+                        .performAsync(new QBEntityCallback<Integer>() {
+                            @Override
+                            public void onSuccess(Integer integer, Bundle bundle) {
+                                QBUnreadMessageHolder.getInstance().setBundle(bundle);
+
+                                ChatDialogsAdapter adapter = new ChatDialogsAdapter(getBaseContext(), QBChatDialogHolder.getInstance().getAllChatDialogs());
+                                firstChatDialogs.setAdapter(adapter);
+                                adapter.notifyDataSetChanged();
+                            }
+
+                            @Override
+                            public void onError(QBResponseException e) {
+                                Log.e("ERROR", e.getMessage());
+                            }
+                        });
+            }
+
+            @Override
+            public void onError(QBResponseException e) {
+                Log.e("ERROR", e.getMessage());
+            }
+        });
+    }
 
     private void createSessionForChat() {
         final ProgressDialog mDialog = new ProgressDialog(ChatDialogsActivity.this);
-        mDialog.setMessage("Please waiting ...");
+        mDialog.setMessage("Xin vui long cho doi ...");
         mDialog.setCanceledOnTouchOutside(false);
         mDialog.show();
 
@@ -103,43 +222,39 @@ public class ChatDialogsActivity extends AppCompatActivity {
                 Log.e("ERROR", ""+e.getMessage());
             }
         });
+    }
 
-        private void loadChatDialogs() {
-            QBRequestGetBuilder requestBuilder = new QBRequestGetBuilder();
-            requestBuilder.setLimit(100);
+    @Override
+    public void processMessage(QBChatMessage qbChatMessage) {
+        QBRestChatService.getChatDialogById(qbChatMessage.getBody()).performAsync(new QBEntityCallback<QBChatDialog>() {
+            @Override
+            public void onSuccess(QBChatDialog qbChatDialog, Bundle bundle) {
+                QBChatDialogHolder.getInstance().putDialog(qbChatDialog);
+                ArrayList<QBChatDialog> adapterSource = QBChatDialogHolder.getInstance().getAllChatDialogs();
+                ChatDialogsAdapter adapters = new ChatDialogsAdapter(getBaseContext(), adapterSource);
+                firstChatDialogs.setAdapter(adapters);
+                adapters.notifyDataSetChanged();
+            }
 
-            QBRestChatService.getChatDialogs(null, requestBuilder).performAsync(new QBEntityCallback<ArrayList<QBChatDialog>>() {
-                @Override
-                public void onSuccess(ArrayList<QBChatDialog> qbChatDialogs, Bundle bundle) {
-                    QBChatDialogHolder.getInstance().putDialogs(qbChatDialogs);
+            @Override
+            public void onError(QBResponseException e) {
+                Log.e("ERROR", ""+e.getMessage());
+            }
+        });
+    }
 
-                    Set<String> setIds = new HashSet<String>();
-                    for (QBChatDialog chatDialog:qbChatDialogs)
-                        setIds.add(chatDialog.getDialogId());
+    @Override
+    public void processError(QBChatException e, QBChatMessage qbChatMessage) {
+        Log.e("ERROR", ""+e.getMessage());
+    }
 
-                    QBRestChatService.getTotalUnreadMessagesCount(setIds, QBUnreadMessageHolder.getInstance().getBundle())
-                            .performAsync(new QBEntityCallback<Integer>() {
-                                @Override
-                                public void onSuccess(Integer integer, Bundle bundle) {
-                                    QBUnreadMessageHolder.getInstance().setBundle(bundle);
+    @Override
+    public void processMessage(String s, QBChatMessage qbChatMessage, Integer integer) {
+        loadChatDialogs();
+    }
 
-                                    ChatDialogsAdapter adapter = new ChatDialogsAdapter(getBaseContext(), QBChatDialogHolder.getInstance().getAllChatDialogs());
-                                    firstChatDialogs.setAdapter(adapter);
-                                    adapter.notifyDataSetChanged();
-                                }
-
-                                @Override
-                                public void onError(QBResponseException e) {
-                                    Log.e("ERROR", e.getMessage());
-                                }
-                            });
-                }
-
-                @Override
-                public void onError(QBResponseException e) {
-                    Log.e("ERROR", e.getMessage());
-                }
-            });
-        }
+    @Override
+    public void processError(String s, QBChatException e, QBChatMessage qbChatMessage, Integer integer) {
+        Log.e("ERROR", e.getMessage());
     }
 }
