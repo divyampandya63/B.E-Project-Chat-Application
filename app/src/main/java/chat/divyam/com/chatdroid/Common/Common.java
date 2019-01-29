a3607135e1a62ad1c020c5c793b46b84cbc62894
divyampandya63 <divyampandya63@gmail.com>
2019-01-29 21:48:49 +0530

    common class added

---
+++ app/src/main/java/chat/divyam/com/chatdroid/Common/Common.java
@@ -0,0 +1,24 @@
+package chat.divyam.com.chatdroid.Common;
+
+import com.quickblox.users.model.QBUser;
+
+import java.util.List;
+
+public class Common {
+
+    public static final String DIALOG_EXTRA = "Dialogs";
+
+    public static String createChatDialogName(List<Integer> qbUsers){
+        List<QBUser> qbUsers1 = QBUsersHolder.getInstance().getUsersByIds(qbUsers);
+        StringBuilder name = new StringBuilder();
+        for (QBUser user:qbUsers1)
+            name.append(user.getFullName()).append(" va ");
+        if (name.length()>30)
+            name = name.replace(30, name.length()-1, " ...");
+        return name.toString();
+    }
+
+    public static boolean isNullOrEmptyString(String content){
+        return (content != null && !content.trim().isEmpty()?false:true);
+    }
+}
